7eda24c9cd234b671bfcf3f5dfbbdf64972bbb55
Justgimmi <sangiovanni.1889445@studenti.uniroma1.it>
2026-05-28 14:59:10 +0200

    Defined A function for Simulation of the process

--- Stuff.R
+++ Stuff.R
@@ -1,22 +1,14 @@
-J <- 10
-I <- diag(1, J)
-D2  <-  matrix(0, J-2, J)
-for (i in 1:nrow(D2)) {
-  D2[i, i:(i+2)] <- c(-1, 2, -1)
-  
-}
-R_J <- matrix(0, J, J)
-for (i in 1:nrow(R_J)) {
-  R_J[i, J - (i-1)] <- 1
-  
-}
 
-K <- t(D2)%*%(D2) + (I- R_J)
-det(K)
-prod(eigen(K)$values[eigen(K)$values > 1e-2])
-install.packages("maotai")
-library(maotai)
-pdeterminant(K)
+# Source file -----
+source("src/Packages.R")
+source("src/Utils.R")
+
+# Try stuff ----
+x <- runif(50) * 2* pi
+x <- c(x)
+par(mfrow = c(1,1))
+Basis <- Basis_Construction(x, 17)
+Basis |> matplot(type = "l") # L = 10
 
 
 
@@ -33,3 +25,66 @@ m <- cbind(2*cos(values), 2*sin(values))
 plot(x, values)
 plot(m)
 ?floor
+
+# data <- read.csv("src/saraghi_final_dataset.csv")
+# data|>
+#   filter(species == "D.sargus") -> Sargus_data
+# final_array <- array(NA, dim = c(19, 2, nrow(Sargus_data)))
+# for (i in 1:nrow(Sargus_data)) {
+#   row_data <- unlist(Sargus_data[i, 8:45])
+#   
+#   final_array[,,i] <- matrix(row_data, nrow = 19, ncol = 2, byrow = TRUE)
+# }
+# library(geomorph)
+# raw_fish <- final_array[,,1]
+# y.gpa<-gpagen(final_array[,,1:3],ProcD = F)
+# raw_fish <- y.gpa$consensus
+# thetas_raw <- atan2(raw_fish[,2], raw_fish[,1])
+# 
+# thetas_raw <- ifelse(thetas_raw < 0, thetas_raw + 2*pi, thetas_raw)
+# radii_raw  <- sqrt(raw_fish[,1]^2 + raw_fish[,2]^2)
+# sort_idx         <- order(thetas_raw)
+# empirical_thetas <- thetas_raw[sort_idx]
+# empirical_radius <- radii_raw[sort_idx]        # Fixed: Now aligns with sorted thetas
+# 
+# fish_sorted    <- raw_fish[sort_idx, ]
+
+
+
+# Simulation ------
+
+
+
+n_points <- 25
+# Create evenly spaced angles from 0 to almost 2*pi
+thetas <- seq(0, 2 * pi, length.out = n_points + 1)[-(n_points + 1)]
+# 
+# B_sim <- Basis_Construction(empirical_thetas, L = L_intervals, degree = degree)
+# tau = 0.2# smoothness
+# K1 = K1_construction(n_basis)
+# K2 = K2_construction(n_basis)
+# #P = (1/(tau^2))*(K1 + 2*K2)
+# P <- (1/(tau^2))*(K1 + 2*K2)
+
+beta_values <- c(0.73331465, 0.27866933, -0.04889285, -0.28155420, -0.53854766, -0.46705397, 
+                    -0.32019879, -0.11568639 ,-0.10122263, 0.29319813, 0.56797438)
+n_points <- 25
+# Create evenly spaced angles from 0 to almost 2*pi
+degree = 3
+sam <- in_model_sample(n = 10, K_l = length(thetas), thetas = thetas, n_int_knots = 8, degree = 3, tau = 0.2, 
+                       beta_values = beta_values)
+
+rot <- sam$mu
+mu_i <- sam$mu_i[,,3]
+X_i <- sam$X[,,3]
+sam$Sigma_e
+# rot <- mu_mean%*%R
+par(mfrow = c(1, 3))
+plot(rot, asp = 1, pch = 21, bg = "darkgreen", main = "Latent mu")
+lines(c(rot[,1], rot[1,1]), c(rot[,2], rot[1,2]), col = "forestgreen", lwd = 2)
+
+plot(mu_i, asp = 1, pch = 21, bg = "darkred", main = "mu for the i-th unit")
+lines(c(mu_i[,1], mu_i[1,1]), c(mu_i[,2], mu_i[1,2]), col = "red", lwd = 2)
+
+plot(X_i, asp = 1, pch = 21, bg = "darkblue", main = "Observed Conf for unit i")
+lines(c(X_i[,1], X_i[1,1]), c(X_i[,2], X_i[1,2]), col = "blue", lwd = 2)
--- src/Utils.R
+++ src/Utils.R
@@ -1,19 +1,7 @@
-source(file = "Packages.R")
+source(file = file.path(getwd(), "src/Packages.R"))
 
 # Basis Construction ------
-# Here we define a function to define the cubic splines 
-
-# 
-# library(mgcv)
-# x <- runif(50) * 2* pi
-# 
-# sx <- s(x, bs = "bs", k = 16, m = 3)
-# smooth <- smoothCon(sx, data = data.frame(x = x), absorb.cons = F)
-# smooth[[1]]$knots # prints the knots
-# par(mfrow = c(1, 1))
-# smooth[[1]]$X |> matplot(type = "l") # shows the basis functions
-# 
-
+# Here we define a function to define the cubic B-splines 
 
 Basis_Construction <- function(thetas, L, degree = 3){
   # I do not like to use already implemented function because they are based
@@ -35,11 +23,11 @@ Basis_Construction <- function(thetas, L, degree = 3){
   
   total_knots <- c(seq(0 - degree*delta, 0, by = delta), knots_base, seq(2*pi , 2*pi + degree*delta, 
                                                                          by = delta))
-  Basis <- splineDesign(total_knots, thetas, ord = degree + 1, outer.ok = FALSE) # Basis construction
+  Basis <- splineDesign(total_knots, thetas, ord = degree + 1, outer.ok = FALSE,
+                        derivs = F) # Basis construction
   # At the End we obtain a matrix of size length(theta) x (L + degree)
   return(Basis)
 }
-
 
 
 # Useful function for K1 and K2 -----
@@ -65,3 +53,124 @@ K2_construction <- function(J){ # symmetry constraint
 }
 
 # Simulation Function ----
+
+# First of all we need a function to sample from a rank deficient normal distribution 
+rmvnorm_rd <- function(n, mu, Precision, tol) {
+  
+  # n: --> how many samples?
+  # mu: --> mean of the normal
+  # Precison: --> Precision matrix
+  # tol: --> degree of tolerance 
+  
+  eig <- eigen(Precision, symmetric = TRUE)
+  
+  keep <- eig$values > tol # define which col to take
+  
+  U <- eig$vectors[, keep, drop = FALSE] # consider just the first keep columns
+  lambda <- eig$values[keep] # consider lambda
+  
+  r <- length(lambda) # rank of the Precision matrix
+  
+  Z <- matrix(rnorm(n * r), nrow = n) # sample from the normal (0, I)
+  
+  X <- t(U%*%diag(1 / sqrt(lambda), r) %*%t(Z)) # reproject back
+  X <- sweep(X, 2, mu, "+") # add the mean
+  
+  return(as.vector(X))
+}
+
+
+
+in_model_sample <- function(n = 1, K_l,  thetas = NA, n_int_knots, degree = 3, tau = 0.1, 
+                            beta_values = NA, lambda = NA, alphas = NA, eta = NA, 
+                            Sigma_e = NA){
+  # K_l --> lanmdarks number 
+  # thetas --> vector of angles
+  # n_int_knots --> number of internal_knots
+  # degree --> degree of the polynomial
+  # tau --> strength of the penalty
+  # thetas block ----
+  if(length(thetas) == 1){
+    thetas <- runif(K_l) *2*pi # sample angles if not given
+  }
+  thetas[thetas < 0]  <- thetas[thetas < 0] + 2*pi # be aware of the domain [0, 2pi]
+  thetas <- sort(thetas) # sort the angles
+  
+  # basis block -----
+  n_basis     <- n_int_knots + degree # final number of basis
+  B_sim <- Basis_Construction(thetas, L = n_int_knots, degree = degree) # Basis lenght(thetas) X n_basis
+  K1 <- K1_construction(n_basis) # smoothness
+  K2 <- K2_construction(n_basis) # symmetry 
+  K <- K1 + 2*K2
+  P <- (1/(tau^2))*K # Precision Matrix
+  if (length(beta_values) == 1 ) { # the user can supply the values of beta
+    beta_values <- rmvnorm_rd(n = 1, mu = rep(0, n_basis), P, tol = 1e-7)
+    
+  }
+  
+  # Mean block ----
+  r <- exp(B_sim %*% as.vector(beta_values)) # radius
+  mu_mean_x <- r*cos(thetas)
+  mu_mean_y <- r*sin(thetas)
+  mu_mean <- cbind(mu_mean_x, mu_mean_y) # mean configuration 
+  R <- array(NA, dim = c(2, 2, n))
+  mu_i <- array(NA, dim = c(K_l, 2, n))
+  if (length(lambda) == 1) {
+    lambda <- runif(n = n)*2*pi # generate rotation angles
+  }
+  
+  if (length(alphas) == 1) {
+    alphas <- rgamma(n = n, shape = 2, rate = 2) # sample the size effect
+    
+  }
+  
+  if (length(eta) == 1) {
+    eta <- matrix(rnorm(n = n*2, sd = 4), ncol = 2) # sample the translation effect 
+    
+  }
+  
+  for (i in 1:n) { # compute rotation matrix 
+    R[,,i] <- matrix(c(cos(lambda[i]), -sin(lambda[i]), sin(lambda[i]), cos(lambda[i])), 
+                     byrow = T, nrow = 2, ncol = 2)
+    eta_matrix <- matrix(eta[i,], nrow = K_l, ncol = 2, byrow = T)
+    mu_i[,,i] <- alphas[i]*mu_mean%*%R[,,i] + eta_matrix # compute the mean configuration for every unit
+  }
+  
+  # Variance block ---- 
+  S <- matrix(c(2.8*1e-4, 0,
+                0, 2.1*1e-4), 2, 2)
+  
+  Sigma_e <- riwish(4, S)# sample measurement error on the p x p
+  #Sigma_e <- diag(2)*0.00035
+  X <- array(NA, dim = c(K_l, 2, n))
+  for (i in 1:n) {
+    # to sample we use the fact that X \sim MN(mu_{i}, I_k, Sigma)
+    Sigmas <- alphas[i]^2 *t(R[,,i])%*%Sigma_e%*%R[,,i]
+    I <- diag(K_l)
+    X[,,i] <- chol(I)%*%matrix(rnorm(n = K_l * 2), nrow = K_l, ncol = 2)%*%t(chol(Sigmas)) + mu_i[,,i]
+    #X[,,i] <- chol(I)%*%matrix(rnorm(n = K_l * 2), nrow = K_l, ncol = 2)%*%t(chol(Sigma_e)) +  mu_i[,,i]
+    }
+  
+  
+  
+  
+  final_param <- list()
+  final_param$mu <- mu_mean
+  final_param$theta <- thetas
+  final_param$r <- r
+  final_param$tau <- tau
+  final_param$degree <- degree
+  final_param$K <- K
+  final_param$n_int_knots <- n_int_knots
+  final_param$R <- R
+  final_param$lambda <- lambda
+  final_param$alphas <- alphas
+  final_param$eta <- eta
+  final_param$mu_i <- mu_i
+  final_param$Sigma_e <- Sigma_e
+  final_param$X <- X
+  
+  return(final_param)
+
+}
+
